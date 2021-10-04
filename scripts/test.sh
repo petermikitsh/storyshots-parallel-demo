8a35302b0c8f3df4e5cfaf9c5c622cb7a118d7b1
Peter Mikitsh <pmikitsh@zuora.com>
2021-10-03 21:17:45 -0700

    chore: commit Strategy B1 implementation

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,11 +11,12 @@ BASEDIR=$(dirname $0)
 REPO_ROOT_DIR="$(dirname $( cd "$BASEDIR" ; pwd -P ))"
 CURR_VERSION=$(node -p "require('${REPO_ROOT_DIR}/package.json').version")
 
+# Only increase concurrency iff you have the computing resources to do so!
 CORES=$(getconf _NPROCESSORS_ONLN)
-HALF_CORES=$(( ($CORES + (2 - 1)) / 2))
+DOUBLE_WORKERS=$(($CORES * 2))
 
 # Run test
-npx --no-install jest 'stories/.*stories' --maxWorkers=$HALF_CORES
+npx --no-install jest 'stories/.*stories' --maxWorkers=$DOUBLE_WORKERS
 
 JEST_EXIT_CODE=$?
 
