3057cac2451d0738e670a9243d751e0f853be854
Peter Mikitsh <pmikitsh@zuora.com>
2021-10-03 18:14:34 -0700

    feat(testCsf): structure jest suite more like storyshots-core
    https://github.com/storybookjs/storybook/blob/a8db9585f07d8cfc7354e68f6b780a6ff6536b38/addons/storyshots/storyshots-core/src/api/snapshotsTestsTemplate.ts#L41-L50

--- .storybook/testCsf.js
+++ .storybook/testCsf.js
@@ -45,70 +45,87 @@ export function testCsf(
     );
   }
 
-  storyEntries
-    .filter(
-      ([exportName]) =>
-        !(Array.isArray(excludeStories)
-          ? excludeStories.includes(exportName)
-          : exportName.matches(excludeStories))
-    )
-    .forEach(([exportName, exported]) => {
-      it(exported.story?.name || exported.storyName || exportName, async () => {
-        expect.hasAssertions();
+  /** @type {import('@storybook/addon-storyshots-puppeteer').ImageSnapshotConfig } */
+  const config = {
+    storybookUrl: "http://localhost:6006",
+    chromeExecutablePath:
+      process.env.SB_CHROMIUM_PATH || "/usr/local/bin/chrome",
+    getGotoOptions: noop,
+    customizePage: asyncNoop,
+    getCustomBrowser: async function () {
+      // add some options "no-sandbox" to make it work properly on some Linux systems as proposed here: https://github.com/Googlechrome/puppeteer/issues/290#issuecomment-322851507
+      const browser = await puppeteer.launch({
+        args: [
+          "--no-sandbox ",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+        executablePath: config.chromeExecutablePath,
+      });
+      return browser;
+    },
+    setupTimeout: 15000,
+    testTimeout: 15000,
+    getMatchOptions: noop,
+    // We consider taking the full page is a reasonable default.
+    getScreenshotOptions: () => ({ fullPage: true, encoding: "base64" }),
+    beforeScreenshot: noop,
+    afterScreenshot: noop,
+    suite: "storyshots",
+    ...global.imageSnapshotOpts,
+  };
 
-        const config = {
-          storybookUrl: "http://localhost:6006",
-          chromeExecutablePath:
-            process.env.SB_CHROMIUM_PATH || "/usr/local/bin/chrome",
-          getGotoOptions: noop,
-          customizePage: asyncNoop,
-          getCustomBrowser: async function () {
-            // add some options "no-sandbox" to make it work properly on some Linux systems as proposed here: https://github.com/Googlechrome/puppeteer/issues/290#issuecomment-322851507
-            const browser = await puppeteer.launch({
-              args: [
-                "--no-sandbox ",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-              ],
-              executablePath: config.chromeExecutablePath,
-            });
-            return browser;
-          },
-          setupTimeout: 15000,
-          testTimeout: 15000,
-          getMatchOptions: noop,
-          // We consider taking the full page is a reasonable default.
-          getScreenshotOptions: () => ({ fullPage: true, encoding: "base64" }),
-          beforeScreenshot: noop,
-          afterScreenshot: noop,
-          ...global.imageSnapshotOpts,
-        };
+  describe(config.suite, () => {
+    const { id: firstStoryId } = storyMeta[Object.keys(storyMeta)[0]];
+    const [kind] = firstStoryId.split("--");
+    describe(kind, () => {
+      // Reuse browser and page for each storybook file
+      let browser;
+      let page;
 
-        const { id } = storyMeta[exportName];
-        const [kind, story] = id.split("--");
+      beforeAll(async () => {
+        browser = await config.getCustomBrowser();
+        page = await browser.newPage();
+      });
 
-        const options = {
-          context: {
-            kind,
-            story,
-            parameters: {},
-          },
-          url: `${config.storybookUrl}iframe.html?id=${id}`,
-        };
-
-        const browser = await config.getCustomBrowser();
-        const page = await browser.newPage();
-        await page.goto(options.url, config.getGotoOptions(options));
-        const element = await config.beforeScreenshot(page, options);
-        const image = await (element || page).screenshot(
-          config.getScreenshotOptions(options)
-        );
-        await config.afterScreenshot({ image, context: options.context });
-        // cleanup
+      afterAll(async () => {
         await page.close();
         await browser.close();
-        // end clean up
-        expect(image).toMatchImageSnapshot(config.getMatchOptions(options));
       });
+
+      storyEntries
+        .filter(
+          ([exportName]) =>
+            !(Array.isArray(excludeStories)
+              ? excludeStories.includes(exportName)
+              : exportName.matches(excludeStories))
+        )
+        .forEach(([exportName, exported]) => {
+          const name = exported.story?.name || exported.storyName || exportName;
+          it(name, async () => {
+            expect.hasAssertions();
+
+            const { id } = storyMeta[exportName];
+            const [, story] = id.split("--");
+
+            const options = {
+              context: {
+                kind,
+                story,
+                parameters: {},
+              },
+              url: `${config.storybookUrl}iframe.html?id=${id}`,
+            };
+
+            await page.goto(options.url, config.getGotoOptions(options));
+            const element = await config.beforeScreenshot(page, options);
+            const image = await (element || page).screenshot(
+              config.getScreenshotOptions(options)
+            );
+            await config.afterScreenshot({ image, context: options.context });
+            expect(image).toMatchImageSnapshot(config.getMatchOptions(options));
+          });
+        });
     });
+  });
 }
